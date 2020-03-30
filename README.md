4b233495101ea2d54af00451fa605f868318c352
Tommy Muir <31448720+Muirey03@users.noreply.github.com>
2020-03-30 22:17:03 +0100

    Bind server to any port

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 RemoteLog supports all the format specifiers of `NSLog` so it should be very easy to migrate to.
 
 - Copy `RemoteLog.h` to `$THEOS/include`
-- Change the ip addresses in the `RemoteLog.h` and `rlogserver.py` files to match your device and computer's ip addresses
+- Change the ip addresses in the `RemoteLog.h` file to match your computer's ip address
 - Include the header in any source files you want to use it in:
 ```
 #include <RemoteLog.h>
--- rlogserver.py
+++ rlogserver.py
@@ -2,16 +2,14 @@
 import logging
 import socket
 
-# change these to match your source (client) IP address and the port in RemoteLog.h
-rlog_ip_address = 'replace with ip'
+# change this to match the port in RemoteLog.h
 rlog_port = 11909
 
 # code is from: https://gist.github.com/majek/1763628
-def udp_server(host=rlog_ip_address, port=rlog_port):
+def udp_server(host="0.0.0.0", port=rlog_port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
-    print("Initiated server on " + host + ":" + str(port))
 
     while True:
         (data, addr) = s.recvfrom(128*1024)
